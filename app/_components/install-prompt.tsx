1f129115a34f4655be1e2642860a28c68051c3d4
conny <jmg41490400@gmail.com>
2025-03-17 22:53:33 +0900

    add web-push, pwa

---
+++ app/_components/install-prompt.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { BeforeInstallPromptEvent } from "@/global";
+import { useEffect, useState } from "react";
+
+export function InstallPrompt() {
+  const [isIOS, setIsIOS] = useState(false);
+  const [isStandalone, setIsStandalone] = useState(false);
+  const [deferredPrompt, setDeferredPrompt] = useState<
+    BeforeInstallPromptEvent | undefined
+  >(undefined);
+
+  useEffect(() => {
+    console.log("InstallPrompt 컴포넌트가 마운트됨");
+    setIsIOS(
+      /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream
+    );
+
+    setIsStandalone(window.matchMedia("(display-mode: standalone)").matches);
+
+    const handleBeforeInstallPrompt = (e: BeforeInstallPromptEvent) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((reg) => console.log("sw worker registered", reg))
+        .catch(() => console.log("failed"));
+    }
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []);
+
+  const handleInstallClick = async () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      await deferredPrompt.userChoice;
+      setDeferredPrompt(undefined);
+    } else {
+      alert("이미 저희 서비스를 설치해주셨어요!");
+    }
+  };
+
+  if (isStandalone) {
+    console.log("이미 설치된 상태 (standalone 모드)");
+    return null;
+  }
+
+  return (
+    <div>
+      <h3>Install App</h3>
+
+      <Button onClick={handleInstallClick}>Add to Home Screen</Button>
+      {isIOS && (
+        <p>
+          To install this app on your iOS device, tap the share button
+          <span role="img" aria-label="share icon">
+            {" "}
+            ⎋{" "}
+          </span>
+          and then "Add to Home Screen"
+          <span role="img" aria-label="plus icon">
+            {" "}
+            ➕{" "}
+          </span>
+          .
+        </p>
+      )}
+    </div>
+  );
+}
