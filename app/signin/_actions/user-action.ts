84b8de281bbfd35798a9ff5a92d51755e3f46bfb
conny <jmg41490400@gmail.com>
2025-03-25 22:46:58 +0900

    change get url

--- app/signin/_actions/user-action.ts
+++ app/signin/_actions/user-action.ts
@@ -3,16 +3,26 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import { Provider } from "@supabase/supabase-js";
-import { headers } from "next/headers";
+
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.endsWith("/") ? url : `${url}/`;
+  return url;
+};
 
 const signInWith = (provider: Provider) => async () => {
-  const origin = (await headers()).get("origin");
   const supabase = await createClient();
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider,
     options: {
-      redirectTo: `${origin}/auth/callback`,
+      redirectTo: `${getURL()}auth/callback`,
     },
   });
 
