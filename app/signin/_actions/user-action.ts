27476459e833b4a7d5e72f0dc6aff24bd3cdce82
conny <jmg41490400@gmail.com>
2025-03-25 22:41:55 +0900

    fix sign in url

--- app/signin/_actions/user-action.ts
+++ app/signin/_actions/user-action.ts
@@ -3,16 +3,16 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import { Provider } from "@supabase/supabase-js";
+import { headers } from "next/headers";
 
 const signInWith = (provider: Provider) => async () => {
+  const origin = (await headers()).get("origin");
   const supabase = await createClient();
-
-  //   const auth_callback_url = `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`;
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider,
     options: {
-      redirectTo: "https://road-marker-web.vercel.app/auth/callback",
+      redirectTo: `${origin}/auth/callback`,
     },
   });
 
